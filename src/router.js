3b29ac6e0b6cd49306b6b7bf145df7de306d0e93
Ebrar Karademir <ebrarcode@hotmail.com>
2024-11-16 13:15:33 +0300

    2

--- src/router.js
+++ src/router.js
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(), // Uses HTML5 history mode
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
