b1695e001b1d312e8b176586da367158443564ce
Ebrar Karademir <ebrarcode@hotmail.com>
2024-11-16 13:05:49 +0300

    gh pages commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/personal-page/', // Replace <REPOSITORY_NAME> with the actual name of your repo
+
 })
