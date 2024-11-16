b698ef4a935714410a0f3fb10d03a2899cf4d528
Ebrar Karademir <ebrarcode@hotmail.com>
2024-11-16 13:11:47 +0300

    delete base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/personal-page/', // Replace <REPOSITORY_NAME> with the actual name of your repo
 
 })
